0be0908d1cc86195b85cbeca58f0cd2999cfc4a5
suhaila <sosoissa04@gmail.com>
2025-02-20 20:16:08 +0200

    lists

--- main.dart
+++ main.dart
@@ -61,6 +61,79 @@ void main() {
   bool isOn = true;
   bool isOof = false;
 
+  //Lists
+  var list = [1, 2, 3];
+  assert(list.length == 3);
+  assert(list[1] == 2);
+
+  list[1] = 1;
+  assert(list[1] == 1);
+
+  var constantList = const [1, 2, 3];
+  // constantList[1] = 1; // This line will cause an error.
+
+  // Growable List
+  List<int> numbers, numbers2, fixedList;
+
+  numbers = [1, 2, 3, 4];
+  numbers2 = []; // Correctly initializing an empty list
+
+  print(numbers); // Output: [1, 2, 3, 4]
+  print(numbers2); // Output: []
+
+  // Adding an item
+  numbers.add(5);
+  numbers2.add(11);
+
+  print(numbers); // Output: [1, 2, 3, 4, 5]
+  print(numbers2); // Output: [11]
+
+  // Accessing items (Fix: Avoid out-of-bounds access)
+  print(numbers[0]); // Output: 1
+
+  // Removing an item
+  numbers.remove(3);
+  print(numbers); // Output: [1, 2, 4, 5]
+
+  // Fixed-length List
+  fixedList = List<int>.filled(
+    3,
+    1,
+  ); // Creates a fixed-size list initialized with 1
+
+  //fixedList.add(5); // Error: Cannot add to a fixed-length list
+  //fixedList.remove(0); // Error: Cannot remove from a fixed-length list
+
+  fixedList[1] = 99;
+  print(fixedList); // Output: [1, 99, 1]
+
+  List<int> var_name1 = List<int>.filled(2, 0);
+  print(var_name1); // Output: [0, 0]
+  var_name1 = List<int>.filled(4, 1);
+  print(var_name1); // Output: [1, 1, 1, 1]
+
+  List<int> number = [10, 20, 30, 40, 50];
+  // Add and remove
+  number.add(60);
+  print(number.length); // 6
+  print(number.indexOf(30)); //2
+  number.remove(30);
+  print(number.indexOf(30)); //-1
+  // Access and modify
+  print(number[1]); // 20
+  number[1] = 25;
+  // Query
+  print(number.contains(48)); // false
+  // Sort and reverse
+  number.sort();
+  print(number.reversed.toList()); // Reversed list
+
+  List items = ["Book", "Car", "Pin"];
+  items.add(12);
+  items.add(2.2);
+  print(items);
+  print(items.runtimeType);
+
   
 
 
